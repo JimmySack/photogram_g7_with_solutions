c609deda3d6db8acf689e7c5835d537af33bcdad
JimmySack <jimmy.sackenheim@kellogg.northwestern.edu>
2017-04-28 11:01:51 -0500

    4.28 in class

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,10 +8,12 @@ class PicturesController < ApplicationController
   end
 
   def index
+    @list_of_photos = Photo.all
     render("pic_templates/index.html.erb")
   end
 
   def show
+    @my_photo = Photo.find(params["the_id"])
     render("pic_templates/show.html.erb")
   end
 
